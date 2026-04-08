96d07f3dd96a663ce6fdd30e7b32dd1541ffbd5f
Antoine LAURENT <antoine.laurent@inclusion.gouv.fr>
2026-04-08 23:10:28 +0200

    script: Improve python script
    I wrote the initial code when I was learning the language, it was
    perfectible

--- script/build_cookbook.py
+++ script/build_cookbook.py
@@ -1,7 +1,9 @@
 import enum
+import glob
 import os
 import shutil
 from operator import itemgetter
+from pathlib import Path
 
 import jinja2
 import yaml
@@ -24,86 +26,86 @@ class MetaEnum(enum.Enum):
     portion = "Portions"
 
 
-ROOT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
-RECIPES_DIR = os.path.join(ROOT_DIR, "recipes")
-TEMPLATES_DIR = os.path.join(ROOT_DIR, "templates")
-# IMGS_DIR = os.path.join(ROOT_DIR, "img")
-SITE_DIR = os.path.join(ROOT_DIR, "_site")
-
-
 def init_ingredients_group_dict():
     return {"list": []}
 
 
-def process_recipe(RECIPE_TEMPLATE, data):
-    recipe_name, _ = os.path.splitext(recipe_file)
-    with open(os.path.join(RECIPES_DIR, recipe_file)) as f:
-        data = yaml.safe_load(f.read())
+class CookBookGenerator:
+    def __init__(self):
+        root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..")
+        self.templates_dir = os.path.join(root_dir, "templates")
+        # imgs_dir = os.path.join(ROOT_DIR, "img")
+        self.site_dir = os.path.join(root_dir, "_site")
 
-    assert data["category"] in CATEGORIES, f"Invalid category {data['category']}"
+        # Load jinja2 templates
+        renderer = jinja2.Environment(loader=jinja2.FileSystemLoader(self.templates_dir))
+        self.recipe_template = renderer.get_template("recipe.html")
+        self.index_template = renderer.get_template("index.html")
 
-    # Replace meta key with label
-    data["meta"] = {MetaEnum[key].value: value for (key, value) in data["meta"].items()}
+    def validate_recipe(self, data):
+        if data["category"] not in CATEGORIES:
+            raise ValueError(f"Invalid category {data['category']}")
 
-    # parse ingredients
-    parsed_ingredients = []
-    group = init_ingredients_group_dict()
-    for ingredient in data["ingredients"]:
-        if "|" in ingredient:
-            # Close previous group
-            if "title" in group:
-                parsed_ingredients.append(group)
-                group = init_ingredients_group_dict()
+    def process_recipe(self, recipe_file):
+        recipe_name = Path(recipe_file).stem
+        with open(recipe_file) as f:
+            data = yaml.safe_load(f.read())
 
-            group["title"] = ingredient[1:]  # without the starting |
-        else:
-            group["list"].append(ingredient)
-    parsed_ingredients.append(group)
-    data["ingredients"] = parsed_ingredients
+        self.validate_recipe(data)
 
-    # data["graph"] = os.path.join(pic_path, data["graph"])
+        # Replace meta key with label
+        data["meta"] = {MetaEnum[key].value: value for (key, value) in data["meta"].items()}
 
-    filename = f"{recipe_name}.html"
-    with open(os.path.join(SITE_DIR, filename), "w") as f:
-        f.write(RECIPE_TEMPLATE.render(**data))
+        # parse ingredients
+        parsed_ingredients = []
+        group = init_ingredients_group_dict()
+        for ingredient in data["ingredients"]:
+            if "|" in ingredient:
+                # Close previous group
+                if "title" in group:
+                    parsed_ingredients.append(group)
+                    group = init_ingredients_group_dict()
 
-    return data["category"], {"name": data["name"], "filename": filename}
+                group["title"] = ingredient[1:]  # without the starting |
+            else:
+                group["list"].append(ingredient)
+        parsed_ingredients.append(group)
+        data["ingredients"] = parsed_ingredients
 
+        # data["graph"] = os.path.join(pic_path, data["graph"])
 
-def process_index(recipes):
-    for cat in CATEGORIES:
-        recipes[cat] = sorted(recipes[cat], key=itemgetter("name"))
-    with open(os.path.join(SITE_DIR, "index.html"), "w") as f:
-        f.write(INDEX_TEMPLATE.render({"recipes": recipes}))
+        html_file = f"{recipe_name}.html"
+        with open(os.path.join(self.site_dir, html_file), "w") as f:
+            f.write(self.recipe_template.render(**data))
+
+        self.recipes[data["category"]].append({"name": data["name"], "filename": html_file})
+
+    def process_index(self):
+        for cat in CATEGORIES:
+            self.recipes[cat] = sorted(self.recipes[cat], key=itemgetter("name"))
+        with open(os.path.join(self.site_dir, "index.html"), "w") as f:
+            f.write(self.index_template.render({"recipes": self.recipes}))
+
+    def build(self):
+        # Create empty _site folder (and remove old content if there was any)
+        shutil.rmtree(self.site_dir, ignore_errors=True)
+        os.makedirs(self.site_dir)
+
+        # Copy stylesheet
+        shutil.copy2(
+            os.path.join(self.templates_dir, "theme.css"),
+            os.path.join(self.site_dir, "theme.css"),
+        )
+
+        # Process recipes files
+        self.recipes = {cat: [] for cat in CATEGORIES}
+        files = glob.glob(r"recipes/[!_]*.yaml")
+        for recipe_file in files:
+            self.process_recipe(recipe_file)
+        self.process_index()
+
+        print("Done")
 
 
 if __name__ == "__main__":
-    os.makedirs(SITE_DIR, exist_ok=True)
-
-    renderer = jinja2.Environment(loader=jinja2.FileSystemLoader(TEMPLATES_DIR))
-    RECIPE_TEMPLATE = renderer.get_template("recipe.html")
-    INDEX_TEMPLATE = renderer.get_template("index.html")
-
-    # copy stylesheet
-    shutil.copy2(
-        os.path.join(TEMPLATES_DIR, "theme.css"),
-        os.path.join(SITE_DIR, "theme.css"),
-    )
-
-    # Parsing recipes files
-    print("Parsing recipes files...")
-    files = [
-        f for f in os.listdir(RECIPES_DIR) if os.path.isfile(os.path.join(RECIPES_DIR, f)) and not f.startswith("_")
-    ]
-
-    recipes = {cat: [] for cat in CATEGORIES}
-
-    # create individuals recipes files
-    for recipe_file in files:
-        category, data = process_recipe(RECIPE_TEMPLATE, recipe_file)
-        recipes[category].append(data)
-
-    # Generate index
-    process_index(recipes)
-
-    print("done")
+    CookBookGenerator().build()
